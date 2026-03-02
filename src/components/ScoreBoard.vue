acce8a953b0886cc97ef89353bcdfff09153d777
Lauren Chan <chanlc@umich.edu>
2026-03-02 10:20:13 +0100

    Add StartScreen and ScoreBoard components

---
+++ src/components/ScoreBoard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="score-board">
+    <h1>Game Over</h1>
+    <p>You scored {{ score }} out of 10</p>
+    <button @click="$emit('restart')">Play Again</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ScoreBoard',
+  props: {
+    score: {
+      type: Number,
+      required: true
+    }
+  },
+  emits: ['restart']
+}
+</script>
