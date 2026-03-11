7e3744c2059bbe420f658ac87ec895c2657a5c59
Lauren Chan <chanlc@umich.edu>
2026-03-11 10:55:02 +0100

    Reflection questions

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { useGameStore } from '../stores/gameStore.js'
 import HomeView from '../views/HomeView.vue'
 import PlayView from '../views/PlayView.vue'
@@ -11,7 +11,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/quizblitz/'),
+  history: createWebHashHistory('/quizblitz/'),
   routes
 })
 
